fb109aeae5d832c2a69c9e0722228104f5328dcb
unknown <1059305248@qq.com>
2026-05-14 14:06:50 +0800

    更新:移除废弃的空值检查函数并简化JSON序列化

--- pytdml/io/tdml_writers.py
+++ pytdml/io/tdml_writers.py
@@ -35,21 +35,6 @@ from typing import Union, Any
 from pytdml.type import TrainingDataset, EOTrainingDataset
 
 
-def _is_empty(obj):
-    return False
-
-
-def remove_empty_values(d):
-    if isinstance(d, dict):
-        return {k: remove_empty_values(v) for k, v in d.items()}
-    elif isinstance(d, list):
-        return [remove_empty_values(v) for v in d]
-    elif isinstance(d, tuple):
-        return tuple(remove_empty_values(v) for v in d)
-    else:
-        return d
-
-
 def write_to_json(
     td: TrainingDataset or EOTrainingDataset,
     file_path: str,
@@ -59,7 +44,4 @@ def write_to_json(
     Writes a TrainingDataset to a JSON file.
     """
     with open(file_path, "w", encoding="utf-8") as f:
-        json.dump(
-            remove_empty_values(td.to_dict()), f, indent=indent, ensure_ascii=False
-        )
-        # json.dump(remove_empty(td.dict()), f, indent=indent, ensure_ascii=False)
+        json.dump(td.to_dict(), f, indent=indent, ensure_ascii=False)
